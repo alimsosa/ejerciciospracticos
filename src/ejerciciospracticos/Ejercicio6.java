5ce11510942c73cff2cede528adf624984317190
alimsosa <42209713+alimsosa@users.noreply.github.com>
2021-04-22 16:30:04 -0300

    #ej6

--- src/ejerciciospracticos/Ejercicio6.java
+++ src/ejerciciospracticos/Ejercicio6.java
@@ -12,39 +12,38 @@ import java.util.Stack;
  *
  * @author estudiante.fit
  */
-public class Ejercicio6 { 
+public class Ejercicio6 {
+
     public LinkedList addTwoNumbers(LinkedList l1, LinkedList l2) {
         LinkedList resultado = new LinkedList();
-        int carry=0;
-        int index=0;
-        int temp1,temp2;
+        int carry = 0;
+        int index = 0;
+        int temp1, temp2;
         int temporal;
-        while (index<l1.size() && index<l2.size()){
-            temp1=(int)l1.get(index);
-            temp2=(int)l2.get(index);
-            temporal=temp1+temp2;
-            
-            if(temporal<10){
-                resultado.addFirst(temporal+carry);
-                carry=0;
-            }
-            else{
-                temporal+=carry;
-                resultado.addFirst(temporal-10);
-                carry=1;
-                
+        while (index < l1.size() && index < l2.size()) {
+            temp1 = (int) l1.get(index);
+            temp2 = (int) l2.get(index);
+            temporal = temp1 + temp2;
+
+            if (temporal < 10) {
+                resultado.addFirst(temporal + carry);
+                carry = 0;
+            } else {
+                temporal += carry;
+                resultado.addFirst(temporal - 10);
+                carry = 1;
             }
 
             index++;
-        } 
-            if(carry!=0){
-             resultado.addFirst(carry);  
-            }
-          
+        }
+        if (carry != 0) {
+            resultado.addFirst(carry);
+        }
+
         return resultado;
-        
+
     }
-    
+
 }
 /*
 
